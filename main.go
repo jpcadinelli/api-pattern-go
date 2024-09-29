6463e70b1f48ef3f170691d0dc6130a8b9999ac2
jpcadinelli <jpcadinelli@gmail.com>
2024-09-29 20:16:52 -0300

    feat: utilização das rotas com gin

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	dbConection "api_pattern_go/api/database/conection"
+	"api_pattern_go/api/routes"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	dbConection.ConnectDatabase()
+
+	router := gin.Default()
+	routes.SetupRoutes(router)
+
+	router.Run(":8080")
 }
