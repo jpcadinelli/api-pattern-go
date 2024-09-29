97cbfe91ff7ec37e93d9e8b91ec14925bb147b50
jpcadinelli <jpcadinelli@gmail.com>
2024-09-29 20:12:51 -0300

    feat: criação da conexão com o banco de dados

---
+++ api/database/conection/conection.go
@@ -0,0 +1,33 @@
+package conection
+
+import (
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"log"
+	"os"
+)
+
+var DB *gorm.DB
+
+func ConnectDatabase() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	dsn := "host=" + os.Getenv("DB_HOST") +
+		" user=" + os.Getenv("DB_USER") +
+		" password=" + os.Getenv("DB_PASSWORD") +
+		" dbname=" + os.Getenv("DB_NAME") +
+		" port=" + os.Getenv("DB_PORT") +
+		" sslmode=disable"
+
+	var err error
+
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("Could not connect to database: %v", err)
+	}
+
+	log.Println("Connected to database")
+}
