70873b16c7f8047c978f3c8e20014c6f379a0077
jpcadinelli <jpcadinelli@gmail.com>
2024-10-16 17:13:08 -0300

    feat: adição validações de erro no crud de testes

--- api/controller/teste/teste.go
+++ api/controller/teste/teste.go
@@ -10,23 +10,47 @@ import (
 
 func Criar(ginctx *gin.Context) {
 	var t models.Teste
+
 	if err := ginctx.ShouldBindJSON(&t); err != nil {
 		ginctx.JSON(http.StatusBadRequest, middleware.NewResponseBridge(err, nil))
 		return
 	}
-	dbConetion.DB.Create(&t)
+
+	tx := dbConetion.DB.Create(&t)
+	if tx.Error != nil {
+		ginctx.JSON(http.StatusInternalServerError, middleware.NewResponseBridge(tx.Error, nil))
+		return
+	}
+
 	ginctx.JSON(http.StatusCreated, middleware.NewResponseBridge(nil, t))
 }
 
 func Visualizar(ginctx *gin.Context) {
 	var t models.Teste
+
 	id := ginctx.Param("id")
-	dbConetion.DB.First(&t, "id = ?", id)
+
+	tx := dbConetion.DB.First(&t, "id = ?", id)
+	if tx.Error != nil {
+		ginctx.JSON(http.StatusInternalServerError, middleware.NewResponseBridge(tx.Error, nil))
+		return
+	}
+	if tx.RowsAffected == 0 {
+		ginctx.JSON(http.StatusNotFound, middleware.NewResponseBridge(nil, nil))
+		return
+	}
+
 	ginctx.JSON(http.StatusOK, middleware.NewResponseBridge(nil, t))
 }
 
 func Listar(ginctx *gin.Context) {
 	var testes []models.Teste
-	dbConetion.DB.Find(&testes)
+
+	tx := dbConetion.DB.Find(&testes)
+	if tx.Error != nil {
+		ginctx.JSON(http.StatusInternalServerError, middleware.NewResponseBridge(tx.Error, nil))
+		return
+	}
+
 	ginctx.JSON(http.StatusOK, middleware.NewResponseBridge(nil, testes))
 }
