b99d49171ec176f13faa87dc06929292f3e8fecc
jpcadinelli <jpcadinelli@gmail.com>
2024-11-26 20:23:03 -0300

    feat: adição do visualizar usuário

--- api/controller/user/user.go
+++ api/controller/user/user.go
@@ -6,7 +6,12 @@ import (
 	"api_pattern_go/api/models"
 	"api_pattern_go/api/services"
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 	"net/http"
+)
+
+const (
+	queryConditionId = "id = ?"
 )
 
 func Criar(ginctx *gin.Context) {
@@ -25,11 +30,32 @@ func Criar(ginctx *gin.Context) {
 		return
 	}
 
-	ginctx.JSON(http.StatusCreated, middleware.NewResponseBridge(nil, u))
+	userResponse := u.UserToDTOResponse()
+	ginctx.JSON(http.StatusCreated, middleware.NewResponseBridge(nil, userResponse))
 }
 
-func Visualizar(_ *gin.Context) {
-	return
+func Visualizar(ginctx *gin.Context) {
+	var u models.User
+
+	idStr := ginctx.Param("id")
+	id, err := uuid.Parse(idStr)
+	if err != nil {
+		ginctx.JSON(http.StatusInternalServerError, middleware.NewResponseBridge(err, nil))
+		return
+	}
+
+	tx := dbConetion.DB.First(&u, queryConditionId, id)
+	if tx.Error != nil {
+		ginctx.JSON(http.StatusInternalServerError, middleware.NewResponseBridge(tx.Error, nil))
+		return
+	}
+	if tx.RowsAffected == 0 {
+		ginctx.JSON(http.StatusNotFound, middleware.NewResponseBridge(nil, nil))
+		return
+	}
+
+	userResponse := u.UserToDTOResponse()
+	ginctx.JSON(http.StatusOK, middleware.NewResponseBridge(nil, userResponse))
 }
 
 func Listar(_ *gin.Context) {
