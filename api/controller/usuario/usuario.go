ee3e4f91cf34ad1fab192cb43f0cf48374c1ce83
jpcadinelli <jpcadinelli@gmail.com>
2024-11-29 17:43:40 -0300

    feat: adição das permissões para os endpoints de usuário

--- api/controller/usuario/usuario.go
+++ api/controller/usuario/usuario.go
@@ -2,9 +2,12 @@ package usuario
 
 import (
 	dbConetion "api_pattern_go/api/database/conection"
+	"api_pattern_go/api/global/enum"
+	"api_pattern_go/api/global/erros"
 	"api_pattern_go/api/middleware"
 	"api_pattern_go/api/models"
 	"api_pattern_go/api/repository"
+	"api_pattern_go/api/service"
 	"api_pattern_go/api/services"
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
@@ -31,6 +34,17 @@ func Criar(ginctx *gin.Context) {
 }
 
 func Visualizar(ginctx *gin.Context) {
+	usuarioLogado, err := service.GetUsuarioLogado(ginctx)
+	if err != nil {
+		ginctx.JSON(http.StatusBadRequest, middleware.NewResponseBridge(err, nil))
+		return
+	}
+
+	if !service.VerificaPermissaoUsuario(*usuarioLogado, enum.PermissaoUsuarioVisualizar) {
+		ginctx.JSON(http.StatusUnauthorized, middleware.NewResponseBridge(erros.ErrUsuarioNaoTemPermissao, nil))
+		return
+	}
+
 	idStr := ginctx.Param("id")
 	id, err := uuid.Parse(idStr)
 	if err != nil {
@@ -48,14 +62,41 @@ func Visualizar(ginctx *gin.Context) {
 	ginctx.JSON(http.StatusOK, middleware.NewResponseBridge(nil, userResponse))
 }
 
-func Listar(_ *gin.Context) {
-	return
+func Listar(ginctx *gin.Context) {
+	usuarioLogado, err := service.GetUsuarioLogado(ginctx)
+	if err != nil {
+		ginctx.JSON(http.StatusBadRequest, middleware.NewResponseBridge(err, nil))
+		return
+	}
+
+	if !service.VerificaPermissaoUsuario(*usuarioLogado, enum.PermissaoUsuarioListar) {
+		ginctx.JSON(http.StatusUnauthorized, middleware.NewResponseBridge(erros.ErrUsuarioNaoTemPermissao, nil))
+		return
+	}
 }
 
-func Atualizar(_ *gin.Context) {
-	return
+func Atualizar(ginctx *gin.Context) {
+	usuarioLogado, err := service.GetUsuarioLogado(ginctx)
+	if err != nil {
+		ginctx.JSON(http.StatusBadRequest, middleware.NewResponseBridge(err, nil))
+		return
+	}
+
+	if !service.VerificaPermissaoUsuario(*usuarioLogado, enum.PermissaoUsuarioAtualizar) {
+		ginctx.JSON(http.StatusUnauthorized, middleware.NewResponseBridge(erros.ErrUsuarioNaoTemPermissao, nil))
+		return
+	}
 }
 
-func Deletar(_ *gin.Context) {
-	return
+func Deletar(ginctx *gin.Context) {
+	usuarioLogado, err := service.GetUsuarioLogado(ginctx)
+	if err != nil {
+		ginctx.JSON(http.StatusBadRequest, middleware.NewResponseBridge(err, nil))
+		return
+	}
+
+	if !service.VerificaPermissaoUsuario(*usuarioLogado, enum.PermissaoUsuarioDeletar) {
+		ginctx.JSON(http.StatusUnauthorized, middleware.NewResponseBridge(erros.ErrUsuarioNaoTemPermissao, nil))
+		return
+	}
 }
--- api/global/enum/permissao.go
+++ api/global/enum/permissao.go
@@ -8,4 +8,9 @@ const (
 	PermissaoPermissaoListar     = "PERMISSAO_LISTAR"
 	PermissaoPermissaoAtualizar  = "PERMISSAO_ATUALIZAR"
 	PermissaoPermissaoDeletar    = "PERMISSAO_DELETAR"
+
+	PermissaoUsuarioVisualizar = "USUARIO_VISUALIZAR"
+	PermissaoUsuarioListar     = "USUARIO_LISTAR"
+	PermissaoUsuarioAtualizar  = "USUARIO_ATUALIZAR"
+	PermissaoUsuarioDeletar    = "USUARIO_DELETAR"
 )
